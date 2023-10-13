a4659ade45407276765ee0bda77cc3d539c53937
barnabas19 <babatundebarnabas@gmail.com>
2023-10-14 00:43:04 +0100

    latest commit

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ import multer from 'multer'
 import authRoutes from './routes/auth.js'
 import refreshTokenRoutes from './routes/refreshToken.js'
 import userRoutes from './routes/users.js'
+import { Server } from "socket.io";
 
 //delete
 // import Network from './models/network.js'
@@ -31,13 +32,49 @@ app.use('/', userRoutes)
 const CONNECTION_URL = process.env.CONNECTION_URL
 const PORT = process.env.PORT || 3000
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
-     .then(() => app.listen(PORT, () => {
-          console.log(`Server running on port ${PORT}`)
+// mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+//      .then(() => app.listen(PORT, () => {
+//           console.log(`Server running on port ${PORT}`)
 
-          // await Network.deleteMany()
-          // await User.deleteMany()
-          // await UserToken.deleteMany()
-          // await Profile.deleteMany()
-     }))
-     .catch((error) => console.log(error.message))
+//           // await Network.deleteMany()
+//           // await User.deleteMany()
+//           // await UserToken.deleteMany()
+//           // await Profile.deleteMany()
+//      }))
+//      .catch((error) => console.log(error.message))
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+     .then(() => {
+          const server = app.listen(PORT, () => {
+              console.log(`Server running on port ${PORT}`)
+
+               // await Network.deleteMany()
+               // await User.deleteMany()
+               // await UserToken.deleteMany()
+               // await Profile.deleteMany()
+          })
+
+          const io = new Server(server)
+
+          let socketsConnected = new Set()
+
+          io.on('connection', onConnected)
+
+          function onConnected(socket){
+               console.log(socket.id)
+               socketsConnected.add(socket.id)
+
+               io.emit('clients-total', socketsConnected.size)
+
+               socket.on('disconnect', () => {
+                    console.log("socket disconnected", socket.id)
+                    socketsConnected.delete(socket.id)
+                    io.emit('clients-total', socketsConnected.size)
+               })
+          }
+
+
+          return server
+     }
+)
+.catch((error) => console.log(error.message))
